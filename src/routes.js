4bdf3e34401f576bed99272047483212ac181ed3
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-03-23 22:32:12 -0300

    feat(buscar): add feature of buscar

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,17 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
+import BuscarController from './app/controllers/BuscarController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/signin', SessionController.store);
 routes.post('/signup', UserController.store);
+
+routes.use(authMiddleware);
+
+routes.get('/buscar', BuscarController.index);
 
 export default routes;
